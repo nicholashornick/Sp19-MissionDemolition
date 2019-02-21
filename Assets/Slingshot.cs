861fb51c23b5e0e6347446d2be4ba6e449ad385f
nicholashornick <33236019+nicholashornick@users.noreply.github.com>
2019-02-21 16:57:40 -0500

    projectile instantiation

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    void Awake()
+    {
+        Transform launchPointTrans =
+transform.Find("LaunchPoint"); // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false
+        );
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    {
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false)
+;    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
